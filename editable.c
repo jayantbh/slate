44797f28475bad718cacc79b3900f1e268d11a1f
Sagnik Chatterjee <chatsagnik@gmail.com>
2016-11-17 18:13:17 +0530

    added insert character and traversal functions

--- editable.c
+++ editable.c
@@ -10,40 +10,89 @@ struct node{
 };
 
 //this link always point to first Link
-struct node *head = NULL;
 
-void display(struct node *node)
+void display(struct node** head_ref)
 {
 	while(node!=NULL){
 		printf("%c", node->data);
 		node = node->next;
 	}
 }
-void insertFirst(char data)
-{
-	
-}
 
-/* the offset marks the position of the cursor from the starting of the head_ref
- we can set the head_ref as the starting position of a particular line or the 
- starting position of the entire document. The first positions will be inserted via insertFirst. */
-void insertAfter(struct node** head_ref, int offset, char newData)
+//push a character at the beginning of the Linked List
+void push(struct node** head_ref, char newData)
 {
 	struct node* newNode = (struct node*) malloc(sizeof(struct node));
 	newNode->data = newData;
 	
-	struct node* temp = (*head_ref);
-}
-
-void deleteFirst()
-{
+	newNode->next = (*head_ref);
+	newNode->prev = NULL;
+		
+	if((*head_ref)! = NULL)
+		(*head_ref)->prev = newNode;
 	
+	(*head_ref) = newNode;
+	
+	return;
 }
 
-/* the offset marks the position of the cursor from the starting of the head_ref
- we can set the head_ref as the starting position of a particular line or the 
- starting position of the entire document */
-void deleteAfter(struct node** head_ref, int offset)
+//insert a character after a given node
+void insertAfter(struct node* prevNode, char newData)
+{
+	if(prevNode == NULL)
+		return; //error has occurred
+	
+	struct node* newNode = (struct node*) malloc(sizeof(struct node));
+	newNode->data = newData;
+	
+	newNode->next = prevNode->next;
+	prevNode->next = newNode;
+	newNode->prev = prevNode;
+	if(newNode->next != NULL)
+		newNode->next->prev = newNode;
+	
+	return;
+}
+
+//append a character to the end of the Linked List
+void append(struct node** head_ref, char newData)
+{
+	struct node* newNode = (struct node*) malloc(sizeof(struct node));
+	newNode->data = newData;
+	newNode->next = NULL;
+	
+	struct node* last = (*head_ref);
+	if((*head_ref) == NULL){
+		newNode->prev = NULL;
+		*head_ref = newNode;
+		return;
+	}
+	while(last->next != NULL)
+		last = last->next;
+	
+	last->next = newNode;
+	newNode->prev = last;
+	
+	return;
+}
+
+//traverse the Linked List to the right, towards the tail
+void goRight(struct node* currNode)
+{
+	if(currNode->next != NULL)
+		currNode = currNode->next;
+	return;
+}
+
+//traverse the Linked List to the left, towards the head
+void goLeft(struct node* currNode)
+{
+	if(currNode->prev != NULL)
+		currNode = currNode->prev;
+	return;
+}
+
+void main()
 {
 	
 }
