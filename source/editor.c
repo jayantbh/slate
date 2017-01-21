367822912f68c473a313523d3cf6cdef1ac2f2fa
soumyarup-paul <paul.soumyarup@gmail.com>
2017-01-21 14:44:55 +0530

    Update editor.c (#9)
    Added functionalities, minor changes

--- source/editor.c
+++ source/editor.c
@@ -9,12 +9,128 @@ which these 4 will have to undergo in order to prove themselves worthy? Stay tun
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX 10
 
 struct node{
 	char data;
 	struct node *next;
 	struct node *prev;
 };
+
+struct urnode{
+	struct node *link;
+	char operation;
+};
+
+int ufront=-1,urear=-1,rfront=-1,rrear=-1;
+struct urnode undostack[10],redostack[10];
+int noofcharacters[1000][1],cursorpos=0;
+
+//undo push
+
+void undoPush(struct node *currNode,char opr){
+	if(ufront==-1){
+		ufront=urear=0;
+	}
+	else{
+		ufront=(ufront+1)%MAX;
+		if(ufront==urear){
+			urear++;
+			free(undostack[ufront].link);
+		}
+	}
+	undostack[ufront].link=currNode;
+	undostack[ufront].operation=opr;
+}
+
+//undo pop
+
+struct urnode undoPop(){
+	int pos=0;
+	/*if(ufront==-1){
+		return ;
+	}*/
+	if(ufront==urear){
+		pos=ufront;
+		ufront=urear=-1;
+	}
+	else{
+		ufront--;
+		if(ufront<0){
+			ufront=9;
+		}
+	}
+	return undostack[pos];
+}
+
+//redo push
+
+void redoPush(struct node *currNode,char opr){
+	if(rfront==-1){
+		rfront=rrear=0;
+	}
+	else{
+		rfront=(rfront+1)%MAX;
+		if(rfront==rrear){
+			rrear++;
+			free(redostack[rfront].link);
+		}
+	}
+	redostack[rfront].link=currNode;
+	redostack[rfront].operation=opr;
+}
+
+//redo pop
+
+struct urnode redoPop(){
+	int pos=0;
+	/*if(ufront==-1){
+		return ;
+	}*/
+	if(rfront==rrear){
+		pos=rfront;
+		rfront=rrear=-1;
+	}
+	else{
+		rfront--;
+		if(rfront<0){
+			rfront=9;
+		}
+	}
+	return redostack[pos];	
+}
+
+//Undo
+
+void undo(){
+	struct urnode unode=undoPop();
+	if(unode.operation=='d'){
+		unode.link->prev->next=unode.link;
+		unode.link->next->prev=unode.link;
+		redoPush(unode.link,'i');
+	}
+	else{
+		unode.link->prev=unode.link->next;
+		unode.link->next=unode.link->prev;
+		redoPush(unode.link,'d');
+	}
+}
+
+//Redo
+
+void redo(){
+	struct urnode rnode=redoPop();
+	if(rnode.operation=='d'){
+		rnode.link->prev->next=rnode.link;
+		rnode.link->next->prev=rnode.link;
+		undoPush(rnode.link,'i');
+	}
+	else{
+		rnode.link->prev=rnode.link->next;
+		rnode.link->next=rnode.link->prev;
+		undoPush(rnode.link,'d');
+	}
+}
 
 //this link always point to first Link
 
@@ -86,6 +202,28 @@ void append(struct node** head_ref, char newData)
 	return;
 }
 
+//traverse the Linked List up, towards the head
+void goUp(struct node* currNode)
+{
+	int i=0;
+	while(currNode->prev != NULL && i<48 && currNode->data != '\n'){
+		currNode = currNode->prev;
+		i++;
+	}
+	return;
+}
+
+//traverse the Linked List down, towards the tail
+void goDown(struct node* currNode)
+{
+	int i=0;
+	while(currNode->next != NULL && i<48 && currNode->data !='\n'){
+		currNode = currNode->next;
+		i++;
+	}
+	return;
+}
+
 //traverse the Linked List to the right, towards the tail
 void goRight(struct node* currNode)
 {
@@ -102,9 +240,9 @@ void goLeft(struct node* currNode)
 	return;
 }
 
-void writeToFile(char *filename)
+void writeToFile(char *filename,struct node* head_ref)
 {
-	FILE *file = fopen(filename, "a+");;
+	FILE *file = fopen(filename, "w+");;
 	char ch;
 	
 	if (!file){
@@ -112,16 +250,28 @@ void writeToFile(char *filename)
 		getchar();
 		return;
 	}
-
-	// For now, if the user does not input anything other than I at the beginning, we will go on asking him to do so or use tilde to escape.
-	printf("Press I and Enter to start entering text and ~ and Enter to stop entering text.\n>> ");
 	
+	struct node* temp = head_ref;
+	while (temp != NULL)
+    {
+        fputc( temp->data, file );
+		temp=temp->next;
+    }
+	
+	fclose(file);
+}
+
+//modify the file using linkedlist
+void operateonlinkedlist(struct node* head_ref){
+	struct node* temp=head_ref;
+	char ch;
 	while(1){
+		printf("Press I and Enter to start entering text and ~ and Enter to Exit.\n>> ");
 		ch=getchar();
 		if(ch == 'I' || ch == 'i'){
 			while(1){
 				if((ch = getchar()) != '~')
-					fputc( ch, file );
+					append( &head_ref,ch );
 				else break;
 			}
 		}
@@ -129,11 +279,9 @@ void writeToFile(char *filename)
 			break;
 		}
 		else{
-			printf("Press I and Enter to start entering text and ~ and Enter to stop entering text.\n>> ");
+			printf("Press I and Enter to start entering text and ~ and Enter to Exit.\n>> ");
 		}
 	}
-	
-	fclose(file);
 }
 
 //take the characters from the file and insert them into a linkedlist while printing them at the same time
@@ -162,14 +310,23 @@ struct node* loadFromFile(char *filename)
 
 int main(int argc, char *argv[])
 {
+	/*
 	// Open the file for writing
 	writeToFile(argv[1]);
+	*/
 	
 	//Load the editable Linked List
 	struct node* head = loadFromFile(argv[1]);
-		
+	
+	//operate on the linked list to insert or delete characters
+	operateonlinkedlist(head);
+	
+	//Write linkedlist content to file
+	writeToFile(argv[1],head);
+	
 	display(head);
 	return 0;
 }
+
 // Green hoti Cabbage, Modi is savage xD
 // Jisse dhaniya samjha, woh pudina nikla; jisse apna samjha woh kameena nikla xD
