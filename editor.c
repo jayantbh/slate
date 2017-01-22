f687301457d8abd50d699e470232b694367d93d9
Sagnik Chatterjee <chatsagnik@gmail.com>
2017-01-22 12:38:08 +0530

    Cleaned up editor.c and made the loading faster

--- editor.c
+++ editor.c
@@ -1,11 +1,3 @@
-/* 
-	After two years of cursing vim and getting too friendly with GUI based text editors, 4 boys turn to the once forsaken text editor for 
-inspiration and guidance on their final year project.
-	Will the CLI text editor Gods look down with benevolence on the return of the prodigal sons, or have they prepared some sort of trial
-which these 4 will have to undergo in order to prove themselves worthy? Stay tuned to know more!!
-
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -16,174 +8,152 @@ struct node{
 	struct node *prev;
 };
 
-void display(struct node* head_ref)
+// return the head of the LinkedList
+struct node* getHead(struct node* currNode)
 {
-	struct node* temp = head_ref;
-	while (temp != NULL)
+	struct node* temp = currNode;
+	while(temp->prev!=NULL){
+		temp = temp->prev;
+	}
+	return temp;
+}
+
+//display characters directly from the Linked List
+void display(struct node* currNode)
+{
+	struct node* temp = getHead(currNode);
+	while (temp->next != NULL)
     {
         printf("%c", temp->data);
         temp = temp->next;
     }
+    return;
 }
 
-//push a character at the beginning of the Linked List
-void push(struct node** head_ref, char newData)
-{
-	struct node* newNode = (struct node*) malloc(sizeof(struct node));
-	newNode->data = newData;
-	
-	newNode->next = (*head_ref);
-	newNode->prev = NULL;
-		
-	if((*head_ref) != NULL)
-		(*head_ref)->prev = newNode;
-	
-	(*head_ref) = newNode;
-	
-	return;
-}
-
-//insert a character after a given node
-void insertAfter(struct node* prevNode, char newData)
-{
-	if(prevNode == NULL)
-		return; //error has occurred
-	
-	struct node* newNode = (struct node*) malloc(sizeof(struct node));
-	newNode->data = newData;
-	
-	newNode->next = prevNode->next;
-	prevNode->next = newNode;
-	newNode->prev = prevNode;
-	if(newNode->next != NULL)
-		newNode->next->prev = newNode;
-	
-	return;
-}
-
-//append a character to the end of the Linked List
-void append(struct node** head_ref, char newData)
-{
-	struct node* newNode = (struct node*) malloc(sizeof(struct node));
-	newNode->data = newData;
-	newNode->next = NULL;
-	
-	struct node* last = (*head_ref);
-	
-	if((*head_ref) == NULL){
-		newNode->prev = NULL;
-		*head_ref = newNode;
-		return;
-	}
-	while(last->next != NULL)
-		last = last->next;
-	
-	last->next = newNode;
-	newNode->prev = last;
-	
-	return;
-}
 
 //traverse the Linked List to the right, towards the tail
-void goRight(struct node* currNode)
+void goRight(struct node* currNode, int n)
 {
-	if(currNode->next != NULL)
+	if(currNode->next != NULL && n>0){
 		currNode = currNode->next;
+		n--;
+	}
 	return;
 }
 
 //traverse the Linked List to the left, towards the head
-void goLeft(struct node* currNode)
+void goLeft(struct node* currNode, int n)
 {
-	if(currNode->prev != NULL)
+	if(currNode->prev != NULL && n<0){
 		currNode = currNode->prev;
+		n++;
+	}
 	return;
 }
 
-void writeToFile(char *filename)
-{
-	FILE *file = fopen(filename, "a+");;
-	char ch;
-	
-	if (!file){
-		printf("File could not be opened. Press any key to continue! \n\a\a");
-		getchar();
-		return;
-	}
+//API METHODS
 
-	// For now, if the user does not input anything other than I at the beginning, we will go on asking him to do so or use tilde to escape.
-	printf("Press I and Enter to start entering text and ~ and Enter to stop entering text.\n>> ");
-	
-	while(1){
-		ch=getchar();
-		if(ch == 'I' || ch == 'i'){
-			while(1){
-				if((ch = getchar()) != '~')
-					fputc( ch, file );
-				else break;
-			}
-		}
-		else if(ch == '~'){
-			break;
-		}
-		else{
-			printf("Press I and Enter to start entering text and ~ and Enter to stop entering text.\n>> ");
-		}
-	}
-	
-	fclose(file);
+//API for cursor handling combining goRight() and goLeft()
+void moveCursor(struct node* currNode, int n)
+{
+	if(n>0) goRight(currNode, n);
+	else goLeft(currNode, n);
+	return;
 }
 
-//take the characters from the file and insert them into a linkedlist while printing them at the same time
-struct node* loadFromFile(char *filename)
+//API for inserting a character after a given node
+void insertCharAfter(struct node* currNode, char newData)
 {
-	struct node* head = NULL;
+	if(currNode == NULL) return; //error has occurred
+	struct node* newNode = (struct node*) malloc(sizeof(struct node));
+	newNode->data = newData;
+	newNode->next = currNode->next;
+	currNode->next = newNode;
+	newNode->prev = currNode;
+	if(newNode->next != NULL) newNode->next->prev = newNode;
+	return;
+}
+
+//API for deleting a character at the current node
+void deleteChar(struct node* currNode)
+{
+	if(currNode == NULL) return; //error has occurred
+	if(currNode->prev!=NULL) currNode->prev->next = currNode->next;
+	if(currNode->next!=NULL) currNode->next->prev = currNode->prev;
+	currNode->prev = NULL;
+	currNode->next = NULL;
+	return;
+}
+
+//Take the characters from the file and insert them into a string (fast loading).
+//Now construct the linkedlist from the string.
+//Return the head pointer of the Linked List.
+struct node* loadFileToList(char *filename)
+{
+	struct node* head = (struct node*) malloc(sizeof(struct node)); struct node* memory1;
+	head->prev = NULL, head->next = NULL;
+	char* list = malloc(100000); char ch;
+	int n = 0, i=0;
 	FILE *file;
-	char ch;
-	file=fopen(filename,"r");
-	
+	file = fopen(filename,"r+");
 	if (!file){
-		printf("File could not be opened. Press any key to continue! \n\a\a");
-		getchar();
-		return;
+		file = fopen(filename,"w+");
+		if(!file){
+			printf("File could not be opened. Press any key to continue! \n\a\a");
+			getchar();
+			return;
+		}
 	}
-	
 	while((ch=fgetc(file))!=EOF){
-		append(&head, ch);
+		list[n++] = (char)ch;
 	}
-	
-	//display(head);
-	return head;
+	fclose(file);
+	list[n] = '\0';
+	head->data = list[0];
+	while(list[i]!='\0'){
+		i++;
+		memory1 = (struct node*)malloc(sizeof(struct node));
+   		memory1->data = list[i];
+   		memory1->prev = head;
+   		memory1->next = NULL;
+   		head->next = memory1;
+	   	head = memory1;
+	}
+	printf("\nFile Loaded!\n");
+	return getHead(head);
+}
+
+//write the linked list back into the file
+void writeBackToFile(struct node* head_ref, char *filename)
+{
+	head_ref = getHead(head_ref);
+	FILE *file = fopen(filename,"w");
+	if (!file){		printf("File could not be opened. Press any key to continue! \n\a\a");		getchar();		return;	}
+	while(head_ref->next != NULL){
+		fputc(head_ref->data, file);
+		head_ref = head_ref->next;
+	}
 	fclose(file);
 }
 
-struct node* getHead(struct node* currNode)
-{
-	while(1){
-		if(currNode-> prev == NULL)
-			return currNode;
-		else
-			currNode = currNode->prev;
-	}
-}
 
+//The MAIN method
 int main(int argc, char *argv[])
 {
-	// Open the file for writing
-	writeToFile(argv[1]);
-	
-	//Load the editable Linked List
-	struct node* curr = loadFromFile(argv[1]);
-		
-	display(curr);
-	
-	//move the current position to the end of the Linked List since that's where the cursor will be
-	while(curr->next != NULL){
-		goRight(curr);
-	}
-	
-	//implementing the arrow captures is beyond my current scope
+	//Load the whole file into a LinkedList and returns the tail pointer
+	struct node* currNode = loadFileToList(argv[1]);
+	display(currNode);
+	/*while(1){
+		Start the front end program somehow.
+		perform operations on the Linked List by capturing keystrokes on the frontend and mapping them to the suitable APIs
+		provided by the backend.
+		API 1. insertCharAfter(currNode,data)
+		API 2. deleteChar(currNode)
+		API 3. moveCursor(currNode, N)
+		API 4. undo()
+		API 5. redo()
+	}*/
+	//Write List back into the file
+	writeBackToFile(currNode, argv[1]);
 	return 0;
 }
-// Green hoti Cabbage, Modi is savage xD
-// Jisse dhaniya samjha, woh pudina nikla; jisse apna samjha woh kameena nikla xD
