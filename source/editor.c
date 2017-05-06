76329ee19fcf940a42ae1552069a8c787895ff0c
Sagnik Chatterjee <sagnik.chatterjee95@gmail.com>
2017-05-06 16:26:01 +0530

    Update editor.c

--- source/editor.c
+++ source/editor.c
@@ -1,4 +1,5 @@
 #include "listfunc.h"
+
 //The MAIN method
 int main(int argc, char *argv[])
 {
@@ -9,11 +10,21 @@ int main(int argc, char *argv[])
 
 	//currNode = moveCursor(currNode,1);//currNode=currNode->next;
 	currNode = insertCharAfter(currNode,'I');//currNode = moveCursor(currNode,1);//currNode=currNode->next;
-
-	currNode = insertCharAfter(currNode,'$');
-	deleteChar(currNode);
 	displayAll(currNode);
-	
+	//printf("\n");
+    currNode = undo(currNode);
+    displayAll(currNode);
+	//printf("yo\n");
+	currNode = insertCharAfter(currNode,'$');
+	displayAll(currNode);
+	//printf("yo2\n%c",currNode->data);
+	currNode = deleteChar(currNode);
+	displayAll(currNode);
+	//printf("yo3\n");
+	currNode = undo(currNode);
+	displayAll(currNode);
+	//printf("yo4\n");
 	writeBackToFile(currNode, argv[1]);
 	return 0;
 }
+
