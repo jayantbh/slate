47f83a5ff72b8ce7c63fd2ea6ec4637ebb8533ed
Sagnik Chatterjee <sagnik.chatterjee95@gmail.com>
2017-05-07 15:17:04 +0530

    Update editor.c

--- source/editor.c
+++ source/editor.c
@@ -3,28 +3,59 @@
 //The MAIN method
 int main(int argc, char *argv[])
 {
-	//Load the whole file into a LinkedList and returns the tail pointer
 	struct node* currNode = loadFileToList(argv[1]);
-	
-	currNode = insertCharAfter(currNode,'H');
-
-	//currNode = moveCursor(currNode,1);//currNode=currNode->next;
-	currNode = insertCharAfter(currNode,'I');//currNode = moveCursor(currNode,1);//currNode=currNode->next;
 	displayAll(currNode);
-	//printf("\n");
-    currNode = undo(currNode);
+	printf("\n");
+	/*We insert ABCDEF then we insert X after C and then we delete E and then we insert Y after F.
+	moveCursor, insert and delete working*/
+	currNode = insertCharAfter(currNode,'A');
     displayAll(currNode);
-	//printf("yo\n");
-	currNode = insertCharAfter(currNode,'$');
+	printf("\n");
+	currNode = insertCharAfter(currNode,'B');
+    displayAll(currNode);
+	printf("\n");
+	currNode = insertCharAfter(currNode,'C');
+    displayAll(currNode);
+	printf("\n");
+	currNode = insertCharAfter(currNode,'D');
+    displayAll(currNode);
+	printf("\n");
+	currNode = insertCharAfter(currNode,'E');
+    displayAll(currNode);
+	printf("\n");
+	currNode = insertCharAfter(currNode,'F');
+    displayAll(currNode);
+	printf("\n");
+	currNode = moveCursor(currNode,-3);
+	currNode = insertCharAfter(currNode,'X');
 	displayAll(currNode);
-	//printf("yo2\n%c",currNode->data);
+	printf("\n");
+	currNode = moveCursor(currNode,2);
 	currNode = deleteChar(currNode);
 	displayAll(currNode);
-	//printf("yo3\n");
-	currNode = undo(currNode);
+	printf("\n");
+	currNode = moveCursor(currNode,1);
+    currNode = insertCharAfter(currNode,'Y');
 	displayAll(currNode);
-	//printf("yo4\n");
+	printf("\n");
+	/*
+    currNode = undo(currNode);
+	move = 0;
+    displayAll(currNode);
+	printf("\n");
+	currNode = insertCharAfter(currNode,'$');
+	displayAll(currNode);
+	printf("\n");
+	currNode = deleteChar(currNode);
+	displayAll(currNode);
+	printf("\n");
+	currNode = undo(currNode);
+	move = 0;
+	displayAll(currNode);
+	printf("\n");*/
 	writeBackToFile(currNode, argv[1]);
 	return 0;
 }
+
+
 
